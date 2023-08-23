2ec41aae5a477de90005386329fa914a8963f689
Sean Eric Thomas <set@seanericthomas.com>
2023-08-23 13:47:23 -0700

    Setup and style header component with nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import Nav from './Nav';
+import '../styles/header.css';
+import logoImage from '../assets/images/zovant-logo-vertical-transparent.png';
 
 export default function Header() {
   return (
-    <header>
-      Zovant
+    <header className="header">
+      <img alt="Zovant" src={logoImage} title="Zovant" />
       <Nav />
     </header>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,23 @@
 import { NavLink } from 'react-router-dom';
+import '../styles/nav.css';
 
 export default function Nav() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/products">Products</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
+    <nav className="nav">
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/products">Products</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
