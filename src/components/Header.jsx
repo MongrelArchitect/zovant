39d5a8540c50405aad303ff48689b4fb237d3265
Sean Eric Thomas <set@seanericthomas.com>
2023-08-25 11:23:50 -0700

    Add gradient to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,11 @@ import logoImage from '../assets/images/zovant-logo-vertical-transparent.png';
 export default function Header() {
   return (
     <header className="header">
-      <img alt="Zovant" src={logoImage} title="Zovant" />
-      <Nav />
+      <div className="header-content">
+        <img alt="Zovant" src={logoImage} title="Zovant" />
+        <Nav />
+      </div>
+      <div className="header-gradient" />
     </header>
   );
 }
