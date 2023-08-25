81275b36c56dd5ab6d385d9712947adb30a50c44
Sean Eric Thomas <set@seanericthomas.com>
2023-08-25 13:52:20 -0700

    Add "contact" section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,17 @@
+import Info from './Info';
+
+import '../styles/contact.css';
+
 export default function Contact() {
   return (
-    <div>
-      Contact
+    <div className="contact">
+      <div className="contact-hero" />
+      <div className="info">
+        <Info
+          heading="Contact"
+          content="Consectetur delectus obcaecati soluta quod a Eum facere fugit quaerat optio eos Vitae debitis labore quas doloremque suscipit Explicabo delectus."
+        />
+      </div>
     </div>
   );
 }
