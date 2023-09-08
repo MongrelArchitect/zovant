1175a67bcc7e53d29144cc087e9a837e034cdd83
Sean Eric Thomas <set@seanericthomas.com>
2023-09-08 15:28:56 -0700

    Add responsive navgation dropdown

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,42 +1,67 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../util/firebase';
 
+import menuIcon from '../assets/images/menu.svg';
+import closeIcon from '../assets/images/close.svg';
+
 export default function Nav({ user }) {
+  const [visible, setVisible] = useState(false);
+
+  const toggleCollapse = () => {
+    setVisible(!visible);
+  };
+
   return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/catalog">Products</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-        {user ? (
-          <>
-            <li>
-              <NavLink to="/dashboard">Dashboard</NavLink>
-            </li>
-            <li>
-              <button
-                className="sign-out"
-                onClick={() => {
-                  signOut(auth);
-                }}
-                type="button"
-              >
-                Sign Out
-              </button>
-            </li>
-          </>
-        ) : null}
-      </ul>
-    </nav>
+    <>
+      <nav className="nav">
+        <ul className={visible ? 'nav-list show' : 'nav-list'} id="navigation">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/catalog">Products</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          {user ? (
+            <>
+              <li>
+                <NavLink to="/dashboard">Dashboard</NavLink>
+              </li>
+              <li>
+                <button
+                  className="sign-out"
+                  onClick={() => {
+                    signOut(auth);
+                  }}
+                  type="button"
+                >
+                  Sign Out
+                </button>
+              </li>
+            </>
+          ) : null}
+        </ul>
+      </nav>
+      <button
+        aria-controls="navigation"
+        aria-expanded={visible ? 'true' : 'false'}
+        aria-label="Toggle navigation"
+        className={visible ? 'toggle-collapse flex-start' : 'toggle-collapse'}
+        onClick={toggleCollapse}
+        type="button"
+      >
+        <img
+          alt=""
+          src={visible ? closeIcon : menuIcon}
+        />
+      </button>
+    </>
   );
 }
