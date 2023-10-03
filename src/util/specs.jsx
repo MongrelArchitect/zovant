e2091a470e62f03d9395a21e594f9a7d4c2765a1
Sean Eric Thomas <set@seanericthomas.com>
2023-10-02 17:05:49 -0700

    Dynamically generate specs table by row length

--- src/util/specs.jsx
+++ src/util/specs.jsx
@@ -1,14 +1,36 @@
-function determineData(row) {
-  if (!row[1]) {
-    // second value in array will be null if it's a heading
-    return <th colSpan="2">{row[0].toUpperCase()}</th>;
+function determineData(row, rowIndex) {
+  if (row.every((cell) => !cell)) {
+    return null;
   }
-  return (
-    <>
-      <th scope="row">{row[0]}</th>
-      <td>{row[1]}</td>
-    </>
-  );
+  if (row[0] && row.slice(1).every((cell) => !cell)) {
+    // value in first cell but not any others = heading
+    return <th colSpan={row.length}>{row[0].toUpperCase()}</th>;
+  }
+  return row.map((cell, index) => {
+    if (index === 0) {
+      return (
+        <th
+          // ignoring this rule since our tables aren't dynamically rerendered
+          // eslint-disable-next-line
+          key={`row${rowIndex}-cell${index}`}
+          className={cell ? 'row-header' : 'row-header empty'}
+          scope="row"
+        >
+          {cell}
+        </th>
+      );
+    }
+    return (
+      <td
+        // ignoring this rule since our tables aren't dynamically rerendered
+        // eslint-disable-next-line
+        key={`row${rowIndex}-cell${index}`}
+        className={!row[0] && !cell ? 'cell empty' : 'cell'}
+      >
+        {cell}
+      </td>
+    );
+  });
 }
 
 export default function generateTable(specsArray) {
@@ -18,7 +40,7 @@ export default function generateTable(specsArray) {
       <tbody>
         {specsArray.map((row) => (
           <tr key={`specs-row-${specsArray.indexOf(row)}`}>
-            {determineData(row)}
+            {determineData(row, specsArray.indexOf(row))}
           </tr>
         ))}
       </tbody>
