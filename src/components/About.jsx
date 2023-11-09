5eccf997bbad352f49ee84a2cfc79f4119463fca
Sean Eric Thomas <set@seanericthomas.com>
2023-11-09 15:18:53 -0800

    Change "about us" copy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
       <div className="info">
         <Info
           heading="About Us"
-          content="Adipisicing ducimus cupiditate ea non sed! Iure nemo totam dolor dolorem eum placeat, nesciunt quos iste sit Cumque laboriosam voluptates repellendus ad explicabo. A deleniti tempore ipsum suscipit nesciunt. Fuga."
+          content="Zovant is a prominent CCTV manufacturer known for its innovative AI-powered products. With a focus on cutting-edge technology, Zovant has carved a niche for itself in the surveillance industry. Their AI-featured CCTV systems offer advanced capabilities, such as intelligent video analytics, facial recognition, and real-time object detection. As a leading player in the market, Zovant continually pushes the boundaries of what's possible in security solutions. Their AI-driven cameras not only provide enhanced monitoring and threat detection but also deliver valuable insights for businesses and organizations. Whether it's in retail, transportation, or residential security, Zovant's AI-integrated CCTV products have garnered praise for their reliability and effectiveness."
         />
       </div>
     </div>
